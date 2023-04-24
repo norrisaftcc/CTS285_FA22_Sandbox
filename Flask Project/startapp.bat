2085f79bf7f17600083ff4240e5c9dadf369d99e
Andrew Norris <norrisa@faytechcc.edu>
2023-04-24 13:49:20 -0400

    cosmetic changes

--- Flask Project/startapp.bat
+++ Flask Project/startapp.bat
@@ -1,4 +1,5 @@
+@echo off 
 rem this is a dos batch file
 echo starting flask app...
-set FLASK_APP = app.py
-python -m flask run 
+rem set FLASK_APP = app.py
+python -m flask --app tastytable run --debug
