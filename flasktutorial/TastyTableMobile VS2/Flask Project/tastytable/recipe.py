36a0b2c2a8b22efe3f682467837a2f2eb2f7079e
Andrew Norris <norrisa@faytechcc.edu>
2023-04-25 13:02:27 -0400

    testing blog component

--- flasktutorial/TastyTableMobile VS2/Flask Project/tastytable/recipe.py
+++ flasktutorial/TastyTableMobile VS2/Flask Project/tastytable/recipe.py
@@ -6,8 +6,8 @@ from flask import (
 )
 from werkzeug.exceptions import abort
 
-from flaskr.auth import login_required
-from flaskr.db import get_db
+from tastytable.auth import login_required
+from tastytable.db import get_db
 
 bp = Blueprint('recipe', __name__)
 
